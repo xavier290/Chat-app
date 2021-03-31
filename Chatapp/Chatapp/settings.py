dd96b11f39b85cf34fbf067281c9217ef2502b59
D0nFrancesco <Luismweber@gmail.com>
2021-03-31 19:07:01 +0200

    User Registration

--- Chatapp/Chatapp/settings.py
+++ Chatapp/Chatapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'chatapi',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Chatapp/Chatapp/views.py
+++ Chatapp/Chatapp/views.py
@@ -2,6 +2,9 @@ from .models import User, Post, Project, Comment, Version, Tag
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from django.views.generic import ListView
+from django.contrib import messages
+from .forms import UserRegisterForm 
+from django.shortcuts import render, redirect
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -16,6 +19,17 @@ class TextSerializer(ModelSerializer):
 
 
 
+def register(request):
+    if request.method = 'Post':
+        form = UserCreationForm(request.Post)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for (username)')
+            return redirect('')#redirection to an url patten maybe homepage
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
 
 
 
@@ -27,7 +41,8 @@ class TextSerializer(ModelSerializer):
 
 
 
-#Comment Serializer
+"""
+Comment Serializer
 @api_view(['POST', ])
 def api_create_Post_view(request):
 
@@ -40,16 +55,16 @@ if request.methode == 'POST':
     if serializer.is_valid():
         serializer.save()
         return = Response(serializer.data, status.HTTP_201_CREATE) 
-        
+       
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-#Comment Serializer
+Comment Serializer
 @api_view(['Delete', ])
 def api_detail_Delete_view(request, slug):
-   account = Account.objects.delete(pk=1)
+account = Account.objects.delete(pk=1)
    post = Post(author=account)
-
+"""
 
 
 
