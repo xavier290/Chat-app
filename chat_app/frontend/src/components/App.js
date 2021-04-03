35efd9924489dca760111138a5605b033ec5291a
xavier290 <xa25alex@gmail.com>
2021-04-02 21:49:42 -0600

    linking sass with babel and webpack

--- chat_app/frontend/src/components/App.js
+++ chat_app/frontend/src/components/App.js
@@ -1,8 +1,18 @@
+import "./styling/app.scss";
+
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-import Header from "./layout/Header";
+// components
 import ChatApp from "./app/chatApp";
+import Register from "./layout/register";
+import Login from "./layout/login";
 
 import { Provider } from "react-redux";
 import store from "../store";
@@ -11,15 +21,20 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <ChatApp />
-          </div>
-        </Fragment>
+        <Router>
+          <Fragment>
+            <div className="container">
+              <Switch>
+                <Route exact path="/admin" component={ChatApp} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
       </Provider>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+export default App;
---
+++ chat_app/frontend/src/components/layout/login.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="login-section">
+          <form action="">
+            <h1>Login</h1>
+          </form>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Login;
