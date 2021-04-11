b70a6c57e96bbaba02b4dfd11c2fe417afd7fdc6
Кирилл <ma-kir.ru@yandex.ru>
2021-04-11 11:46:46 +0300

    2nd channels tutorial files added

--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'chat/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -1,12 +1,18 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+  'http': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
